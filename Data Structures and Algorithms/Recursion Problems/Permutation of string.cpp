e8bcc6fce1fe1b8b17e26ce5821fd6425f58e04f
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2023-10-29 01:42:14 +0530

    Create Permutation of string.cpp

---
+++ Data Structures and Algorithms/Recursion Problems/Permutation of string.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void printPermutation(string str, int index){
+
+    // base case
+
+    if(index >= str.length()){
+        cout <<str <<" ";
+        return;
+    }
+
+    for(int j= index; j <str.length(); j++){
+        swap(str[index], str[j]);
+        // recursive call
+        printPermutation(str, index+1);
+    }
+}
+
+int main(){
+
+    string str = "abc";
+    int index = 0;
+    printPermutation(str, index);
+}
