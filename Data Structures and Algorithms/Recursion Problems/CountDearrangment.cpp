6a36f2c176d4e32e856454f6232754ca455a1157
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2023-11-03 01:41:47 +0530

    Create CountDearrangment.cpp

---
+++ Data Structures and Algorithms/Recursion Problems/CountDearrangment.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int solve(int n){
+    // base case
+
+    if(n == 1){
+        return 0;
+    }
+
+    if(n == 2){
+        return 1;
+    }
+
+    int ans = (n-1)* (solve(n-1) + (solve(n-2)));
+}
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    cout<< solve(n)<<endl;
+
+}
