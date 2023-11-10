a43275982a2c63a54c6c8afe22fb980e9ad815a9
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2023-11-10 23:59:20 +0530

    Create check Palindrome.cpp

---
+++ Data Structures and Algorithms/check Palindrome.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int findLength(char ch[], int size){
+    
+    int index = 0;
+
+    while(ch[index] != '\0'){
+        index++;
+    }
+    return index;
+}
+
+bool checkPalindrome(char ch[], int n){
+
+    int i = 0;
+    int j = n-1;
+
+    while(i <= j){
+        if(ch[i] == ch[j]){
+            i++;
+            j--;
+            
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+
+    char ch[100];
+    cin.getline(ch, 100);
+
+    int len = findLength(ch, 100);
+
+    bool isPalindrome = checkPalindrome(ch, len);
+
+    if(isPalindrome){
+        cout<<"Valid Palindrome "<<endl;
+    }
+    else{
+        cout <<"Invalid Palindrome "<<endl;
+    }
+
+
+}
