2bd1c4365965017e9935eb0f5f7152111e6141bf
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-01-05 12:10:50 +0530

    Create RotateArray.cpp

---
+++ Data Structures and Algorithms/RotateArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void rotate(vector<int>& nums, int k){
+    vector<int> temp(nums.size());
+
+    for(int i = 0; i < nums.size(); i++){
+        temp[(i + k) % nums.size()] = nums[i];
+    }
+    nums=temp;
+}
+
+int main(){
+
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
+
+    cout<<"Original Array: ";
+    for(int num: nums){
+        cout << num <<" ";
+    }
+    cout<<endl;
+
+    rotate(nums, k);
+
+    cout<<"Rotated array: ";
+    for(int num: nums){
+        cout<< num <<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
