d987d7f239edbc4e430c003384d479c27961084f
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-03-02 11:08:36 +0530

    Update 3SUM.cpp

--- Data Structures and Algorithms/Imp Question's/3SUM.cpp
+++ Data Structures and Algorithms/Imp Question's/3SUM.cpp
@@ -1,7 +1,7 @@
 // This solution will give a TLE
 // time complexity: O(n^3)
 
-
+// Approach 1: Brute force
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
@@ -19,5 +19,31 @@ public:
         }
         vector<vector<int>> ans(st.begin() ,st.end());
         return ans;
+    }
+};
+
+// Approach 2: Hashing
+// This is solotion might give TLE
+// time complexity: O(n^2)
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> st;
+        for(int i=0; i< nums.size(); i++) {
+            set<int> hashset;  //Empty set
+            for(int j= i+1; j<nums.size(); j++){
+                int third = -(nums[i] + nums[j]);
+                if(hashset.find(third) != hashset.end()){
+                    vector<int> temp = {nums[i] ,nums[j], third};
+                    sort(temp.begin() ,temp.end());
+                    st.insert(temp);
+                }
+                hashset.insert(nums[j]);
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+
     }
 };
