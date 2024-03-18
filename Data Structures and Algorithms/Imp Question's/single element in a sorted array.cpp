af3876fe722af6483639b32db8f610390dc7cad3
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-03-18 20:07:07 +0530

    Create single element in a sorted array.cpp

---
+++ Data Structures and Algorithms/Imp Question's/single element in a sorted array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+
+        for(int i=0; i<n; i++) {
+
+            // checking for first index
+            if(i == 0){
+                if(nums[i] != nums[i+1])
+                    return nums[i];
+            }
+
+            // cheking for last index
+            else if(i == n-1){
+                if(nums[i] != nums[i-1])
+                    return nums[i];
+            }
+
+            else{
+                if(nums[i] != nums[i-1] && nums[i] != nums[i+1])
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
