588d867d6fd705282344f596b1de2b82a2f4386b
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-07-14 18:11:01 +0530

    Update Bubble Sort.cpp

--- Data Structures and Algorithms/Imp Question's/Bubble Sort.cpp
+++ Data Structures and Algorithms/Imp Question's/Bubble Sort.cpp
@@ -1,9 +1,9 @@
-#Bubble Sort Algorithm
+*Bubble Sort Algorithm
 In Bubble Sort algorithm, 
 
-##traverse from left and compare adjacent elements and the higher one is placed at right side. 
-##In this way, the largest element is moved to the rightmost end at first. 
-##This process is then continued to find the second largest and place it and so on until the data is sorted.
+traverse from left and compare adjacent elements and the higher one is placed at right side. 
+In this way, the largest element is moved to the rightmost end at first. 
+This process is then continued to find the second largest and place it and so on until the data is sorted.
 
 #include<iostream>
 using namespace std;
