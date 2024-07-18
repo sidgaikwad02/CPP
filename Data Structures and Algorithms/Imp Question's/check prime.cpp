daed8c0a7753b888b8c8fe040e8629c2b294799b
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-07-18 19:22:35 +0530

    Create check prime.cpp

---
+++ Data Structures and Algorithms/Imp Question's/check prime.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int checkPrime(int num)
+{
+    if(num < 2){
+        return 0;
+    }
+    else{   
+       int x = num/2;
+        for(int i = 2; i < x; i++)
+        {
+            if(num % i == 0)
+            {
+                return 0;
+            }
+        }
+    }
+    
+    return 1;
+}
+
+int main()
+{
+    int a = 1, b = 100;
+    
+    for(int i=a; i <= b; i++){
+        if(checkPrime(i))
+            cout<<i<<" ";
+    }
+ 
+    return 0;
+}
+//Time Complexity: O(N^2)
+//Space Complexity O(1)
