2912d326c050f36d9faff9e457871ff3b63d0433
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-08-02 18:40:11 +0530

    Update Kth max and min element array.cpp

--- Data Structures and Algorithms/Imp Question's/Kth max and min element array.cpp
+++ Data Structures and Algorithms/Imp Question's/Kth max and min element array.cpp
@@ -1,34 +1,33 @@
-#include<iostream>
-#include<vector>
-#include<set>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n;
-    cout<<"Enter a number: ";
-    cin>>n;
-    
-    vector<int>arr(n);
-    
-    for(int i = 0; i < n; i++) 
-        cin>>arr[i];
-        
+    cout<<"Enter the number: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
     int k;
-    cout<<"Enter a k element: ";
-    cin>>k;
-    
+    cin >> k;
+
     set<int> s(arr.begin(), arr.end());
-    
+
     if(k > s.size()) {
-        cout<<"k is larger than the number of unique element"<<endl;
+        cout << "K is larger than the number of unique elements\n";
         return 1;
     }
-    
-    auto itr = s.begin();
-    advance(itr, k-1);
-    cout<<"Minimum: " << *itr <<endl;
-    
+
+    auto itr = s.begin(); // s.begin() returns an iterator to the first element in the set
+    advance(itr, k - 1); // advance iterator to the k-th element (k-th smallest)
+    cout << "Minimum: " << *itr << "\n";
+
     itr = s.begin();
-    advance(itr, s.size()-k);
-    cout<<"MAximum: "<< *itr <<endl;
+    advance(itr, s.size() - k); // advance iterator to the (n-k+1)-th element (k-th largest)
+    cout << "Maximum: " << *itr << "\n";
+
+    return 0;
 }
