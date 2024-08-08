7c81d7c24f9b2231485339aa315791c0acb6ed5d
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-08-08 18:25:13 +0530

    Update Remove duplicates from sorted array.cpp

--- Data Structures and Algorithms/Imp Question's/Remove duplicates from sorted array.cpp
+++ Data Structures and Algorithms/Imp Question's/Remove duplicates from sorted array.cpp
@@ -12,3 +12,28 @@ public:
         return a;
     }
 };
+
+// Method : 2
+
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int main() {
+    int arr[] = {10,20,20,30,40,40,50,60,60};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    unordered_map<int ,int> mp;
+    
+    for(int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+    }
+    
+    for(auto it : mp) {
+        if(it.second > 1) {
+            cout<<it.first<<" ";
+        }
+    }
+    cout<<endl;
+}
