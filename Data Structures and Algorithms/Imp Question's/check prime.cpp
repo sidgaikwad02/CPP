7aa82c9bbcf881daf4dbfa10f12712c4a7a2c48f
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-08-23 18:53:35 +0530

    Update check prime.cpp

--- Data Structures and Algorithms/Imp Question's/check prime.cpp
+++ Data Structures and Algorithms/Imp Question's/check prime.cpp
@@ -1,32 +1,31 @@
 #include<iostream>
 using namespace std;
 
+bool isPrime(int n) {
+    int count = 0;
+    
+    if(n < 2) 
+        return false;
+    
+    for(int i = 2;i < n; i++) {
+            if(n % i == 0) 
+                return false;
+        }
+    
+    return true;
+}
+
 int main() {
     int n;
     cout<<"Enter a number: ";
     cin >> n;
     
-    bool isPrime = true;
-    
-    if(n < 2) {
-        isPrime = false;
+    if(isPrime(n)) {
+        cout<<"Prime number";
     }
-    else{
-       for(int i = 2; i < n; i++) {
-        if(n % i == 0) {
-            isPrime = false;
-        }
-    } 
-    
-}
-
-if(isPrime) {
-    cout<<"The number is Prime";
-}
-else {
-    cout<<"Number is not Prime";
-}
-    
+    else {
+        cout<<"Not Prime number";
+    }
 }
 //Time Complexity: O(N)
 //Space Complexity O(1)
