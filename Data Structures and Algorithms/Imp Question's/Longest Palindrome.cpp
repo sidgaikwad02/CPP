3be892378e2b3c8375a00b3626a9a37a9850d8fc
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-08-25 23:03:37 +0530

    Update Longest Palindrome.cpp

--- Data Structures and Algorithms/Imp Question's/Longest Palindrome.cpp
+++ Data Structures and Algorithms/Imp Question's/Longest Palindrome.cpp
@@ -5,7 +5,7 @@ bool isPalindrome(int n) {
     int rev = 0, temp = n;
     
     while(temp > 0) {
-        rev = rev * 10 + temp% 10;
+        rev = rev * 10 + temp % 10;
         temp /= 10;
     }
     
@@ -14,14 +14,15 @@ bool isPalindrome(int n) {
 
 int main() {
     int arr[] = {1, 121, 55551, 545545, 10111, 90};
-    int n = sizeof(arr)/ sizeof(arr[0]);
+    int n = sizeof(arr)/sizeof(arr[0]);
     int maxPalindrome = -1;
     
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++ ){
         if(isPalindrome(arr[i]) && arr[i] > maxPalindrome) {
-        maxPalindrome = arr[i];
+            maxPalindrome = arr[i];
+        }
     }
-    }
-
-    cout<<maxPalindrome<<endl;
+    
+    cout<<"Longest palindrome in a array is: "<<maxPalindrome<<endl;
+    
 }
