b4c060da984fc15fef4e33104935b834d2e69514
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-09-14 19:31:24 +0530

    Update Move all negative element.cpp

--- Data Structures and Algorithms/Imp Question's/Move all negative element.cpp
+++ Data Structures and Algorithms/Imp Question's/Move all negative element.cpp
@@ -1,49 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void shiftall(int arr[], int left, int right) {
-    while(left <= right) {
-        if(arr[left] < 0 && arr[right] < 0) {
-            left += 1;
-        }
-        
-        else if(arr[left] > 0 && arr[right] > 0) {
-            right -= 1;
-        }
-        
-        else if(arr[left] < 0 && arr[right] > 0) {
-            int temp = arr[left];
-            arr[left] = arr[right];
-            arr[right] = temp;
-            left += 1;
-            right -= 1;
-        }
-        else{
-            left += 1;
-            right -= 1;
+void move(int arr[], int n) {
+    int j = 0;
+    
+    for(int i = 0; i < n; i++) {
+        if(arr[i] < 0) {
+            if(i != j)
+            swap(arr[i], arr[j]);
+            j++;
         }
     }
 }
-
-void print(int arr[], int right) {
-    for(int i = 0; i <= right; i++){
-         cout<< arr[i] <<" ";
-    }
-   cout<<endl;
-}
-
 
 int main() {
-    int n;
-    cout<<"Enter the number: ";
-    cin >> n;
+    int arr[] = {8,6,-8,-1,-74};
+    int n = 5;
     
-    int arr[n];
-    for(int i = 0; i < n; i++) 
-        cin >>arr[i];
-    
-    shiftall(arr, 0, n-1);
-    print(arr, n-1);
-    
-    return 0;
+    move(arr, n);
+    for(int i = 0; i <n; i++)
+    cout<<arr[i]<<" ";
 }
