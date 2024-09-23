36129df0cf268902b9cde51a42919a30d4c761f8
Siddharth Gaikwad <122147175+sidgaikwad02@users.noreply.github.com>
2024-09-23 18:25:41 +0530

    Update Reverse string.cpp

--- Data Structures and Algorithms/Imp Question's/Reverse string.cpp
+++ Data Structures and Algorithms/Imp Question's/Reverse string.cpp
@@ -21,6 +21,29 @@ int main() {
     getline(cin, str);  // To input string with spaces
     
     reverseString(str);
+
+    // stack
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+    string str = "God";
+    stack<char> st;
+    
+    for(int i=0; i<str.length(); i++) {
+        char ch = str[i];
+        st.push(ch);
+    }
+    
+    while( !st.empty()) {
+        cout<< st.top();
+        st.pop();
+    }
+    cout<<endl;
+    
+}
     
     cout << "Reversed string: " << str << endl;
     
